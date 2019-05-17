19d7969839526c57828bbcd3e1400bc8377b5e55
chao <zhouzechao10@163.com>
2019-05-17 17:03:31 +0800

    试试断点续录

--- app/src/main/java/viewset/com/kkcamera/view/activity/GLCameraActivity.java
+++ app/src/main/java/viewset/com/kkcamera/view/activity/GLCameraActivity.java
@@ -2,6 +2,8 @@ package viewset.com.kkcamera.view.activity;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,6 +16,7 @@ import butterknife.OnClick;
 import butterknife.Unbinder;
 import viewset.com.kkcamera.R;
 import viewset.com.kkcamera.view.camera.KKGLSurfaceView;
+import viewset.com.kkcamera.widget.CircularProgressView;
 
 public class GLCameraActivity extends AppCompatActivity {
 
@@ -23,18 +26,26 @@ public class GLCameraActivity extends AppCompatActivity {
     @BindView(R.id.iv_pic)
     ImageView iv_pic;
 
+    @BindView(R.id.record)
+    CircularProgressView record;
+
     private Unbinder unbinder;
 
     private boolean isRecording = false;
+
+    private static final int MAXTIME = 20000;//最长录制20s
+    private int currentTime = 0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera2);
         unbinder = ButterKnife.bind(this);
+
+        record.setTotal(MAXTIME);
     }
 
-    @OnClick({R.id.bt_switch, R.id.takephoto,R.id.record})
+    @OnClick({R.id.bt_switch, R.id.takephoto, R.id.record})
     public void onViewClick(View view) {
         switch (view.getId()) {
             case R.id.bt_switch:
@@ -56,14 +67,31 @@ public class GLCameraActivity extends AppCompatActivity {
                 break;
             case R.id.record:
                 if(isRecording){
+                    mHandler.removeMessages(0);
+                    record.pauce();
                     kkcamera.stopRecord();
                 }else {
+                    mHandler.sendMessageDelayed(mHandler.obtainMessage(0), 50);
                     kkcamera.startRecord();
                 }
                 isRecording = !isRecording;
                 break;
         }
     }
+
+    Handler mHandler = new Handler() {
+        @Override
+        public void dispatchMessage(Message msg) {
+            if (currentTime <= MAXTIME && isRecording) {
+                record.setProcess(currentTime);
+                currentTime += 50;
+                mHandler.sendMessageDelayed(mHandler.obtainMessage(0), 50);
+            } else {
+                isRecording = false;
+                kkcamera.stopRecord();
+            }
+        }
+    };
 
     @Override
     public void onResume() {
